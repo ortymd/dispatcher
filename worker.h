d633fc9d2be086be12dd408d5b735999384ae893
Maksym Ivasiv <maks1404@gmail.com>
2021-03-10 11:12:37 +0200

    unique_ptr

--- worker.h
+++ worker.h
@@ -1,11 +1,11 @@
 /*
 	worker.h - Header file for the Worker class.
-
+	
 	Revision 0
-
+	
 	Notes:
-			-
-
+			- 
+			
 	2016/11/19, Maya Posch
 	(c) Nyanko.ws
 */
@@ -23,19 +23,19 @@
 using namespace std;
 
 
-class Worker {
+class Worker : public enable_shared_from_this<Worker>{
 	condition_variable cv;
 	mutex mtx;
 	unique_lock<mutex> ulock;
-	AbstractRequest* request;
+	unique_ptr<AbstractRequest> request;
 	bool running;
 	bool ready;
-
+	
 public:
 	Worker() { running = true; ready = false; ulock = unique_lock<mutex>(mtx); }
 	void run();
 	void stop() { running = false; }
-	void setRequest(AbstractRequest* request) { this->request = request; ready = true; }
+	void setRequest(unique_ptr<AbstractRequest> request) { this->request = move(request); ready = true; }
 	void getCondition(condition_variable* &cv);
 };
 
