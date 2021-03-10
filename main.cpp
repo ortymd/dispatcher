784fe3a89ef420c837947ec10dc72f85e87e4134
Maksym Ivasiv <maks1404@gmail.com>
2021-03-10 11:49:35 +0200

    removed whitespaces

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 /*
 	main.cpp - Main file for the Dispatcher demo application.
-	
-	Revision 0 
-	
+
+	Revision 0
+
 	Features:
-			- 
-			
+			-
+
 	Notes:
-			- 
-			
+			-
+
 	2016/11/19, Maya Posch
 	(c) Nyanko.ws
 */
@@ -22,7 +22,6 @@
 #include <csignal>
 #include <thread>
 #include <chrono>
-#include <memory>
 
 using namespace std;
 
@@ -47,31 +46,29 @@ void logFnc(string text) {
 int main() {
 	// Install signal handler.
 	signal(SIGINT, &sigint_handler);
-	
+
 	// Initialise the dispatcher with 10 worker threads.
 	Dispatcher::init(10);
-	
+
 	cout << "Initialised.\n";
-	
+
 	// Generate requests in a continuous loop until terminated with SIGINT or
 	// limit has been reached.
 	int cycles = 0;
-	//Request* rq = 0;
-	unique_ptr<Request> rq;
+	Request* rq = 0;
 	while (!signal_caught && cycles < 50) {
-		//rq = new Request();
-		rq = make_unique<Request>();
+		rq = new Request();
 		rq->setValue(cycles);
 		rq->setOutput(&logFnc);
-		Dispatcher::addRequest(move(rq));
+		Dispatcher::addRequest(rq);
 		cycles++;
 	}
-	
+
 	this_thread::sleep_for(chrono::seconds(5));
-	
+
 	// Cleanup.
 	Dispatcher::stop();
 	cout << "Clean-up done.\n";
-	
+
 	return 0;
 }
